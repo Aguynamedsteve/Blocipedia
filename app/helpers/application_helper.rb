c5b2c13471ef5d5c642ff176f38c3db60eb213e6
Steve <steve.barnesjr@yahoo.com>
2014-04-27 16:05:58 -0700

    Fixed git merge conflict hiccup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def markdown(text)
-<<<<<<< HEAD
   renderer = Redcarpet::Render::HTML.new
   extensions = {
     fenced_code_blocks: true,
@@ -13,19 +12,5 @@ module ApplicationHelper
   }
   redcarpet = Redcarpet::Markdown.new(renderer, extensions)
   (redcarpet.render text).html_safe
-=======
-    renderer = Redcarpet::Render::HTML.new
-    extensions = {
-      fenced_code_blocks: true,
-      tables: true,
-      autolink: true,
-      strikethrough: true,
-      underline: true,
-      highlight: true,
-      quote: true
-    }
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    (redcarpet.render text).html_safe
->>>>>>> collabo
   end
 end
